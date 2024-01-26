37f985ecebd425f7416718e9a37556be3a5d2dd0
Ales Pour <ales.pour@bonitoo.io>
2024-01-26 16:17:37 +0100

    fix: add options pattern support

--- influxdb3/options.go
+++ influxdb3/options.go
@@ -28,7 +28,7 @@ import (
 
 // QueryOptions holds options for query
 type QueryOptions struct {
-	// Database for querying. Use in `QueryWithOptions` method to override default database in `ClientConfig`.
+	// Database for querying. Use to override default database in `ClientConfig`.
 	Database string
 
 	// Query type.
@@ -37,11 +37,11 @@ type QueryOptions struct {
 
 // WriteOptions holds options for write
 type WriteOptions struct {
-	// Database for writing. Use in `WriteWithOptions` methods to override default database in `ClientConfig`.
+	// Database for writing. Use to override default database in `ClientConfig`.
 	Database string
 
-	// Precision to use in writes for timestamp.
-	// Default `lineprotocol.Nanosecond`
+	// Precision of timestamp to use when writing data.
+	// Default value: lineprotocol.Nanosecond
 	Precision lineprotocol.Precision
 
 	// Tags added to each point during writing. If a point already has a tag with the same key, it is left unchanged.
@@ -54,6 +54,7 @@ type WriteOptions struct {
 	//  	Database:     "my-database",
 	//  })
 	//
+	// Example:
 	//  options := WriteOptions{
 	//  	DefaultTags: map[string]string{
 	//  		"rack": "main",
@@ -77,7 +78,6 @@ type WriteOptions struct {
 	//  		DefaultTags: map[string]string{
 	//  			"rack": "main",
 	//  		},
-	//  		Precision: lineprotocol.Millisecond,
 	//  	},
 	//  })
 	//
@@ -88,7 +88,7 @@ type WriteOptions struct {
 	//  c.WritePoints(context.Background(), p)
 	DefaultTags map[string]string
 
-	// Write body larger than the threshold is gzipped. 0 to don't gzip at all
+	// Write body larger than the threshold is gzipped. 0 for no compression.
 	GzipThreshold int
 }
 
@@ -101,4 +101,87 @@ var DefaultQueryOptions = QueryOptions{
 var DefaultWriteOptions = WriteOptions{
 	Precision:     lineprotocol.Nanosecond,
 	GzipThreshold: 1_000,
+}
+
+// Option is a functional option type that can be passed to Client.Query and Client.Write methods.
+type Option func(o *options)
+
+// QueryOption is a functional option type that can be passed to Client.Query.
+// Available options:
+//   - WithDatabase
+//   - WithQueryType
+type QueryOption = Option
+
+// WriteOption is a functional option type that can be passed to Client.Write methods.
+// Available options:
+//   - WithDatabase
+//   - WithPrecision
+//   - WithGzipThreshold
+//   - WithDefaultTags
+type WriteOption = Option
+
+// WithDatabase is used to override default database in Client.Query and Client.Write methods.
+func WithDatabase(database string) Option {
+	return func(o *options) {
+		o.QueryOptions.Database = database
+		o.WriteOptions.Database = database
+	}
+}
+
+// WithQueryType is used to override default query type in Client.Query method.
+func WithQueryType(queryType QueryType) Option {
+	return func(o *options) {
+		o.QueryType = queryType
+	}
+}
+
+// WithPrecision is used to override default precision in Client.Write methods.
+func WithPrecision(precision lineprotocol.Precision) Option {
+	return func(o *options) {
+		o.Precision = precision
+	}
+}
+
+// WithGzipThreshold is used to override default GZIP threshold in Client.Write methods.
+func WithGzipThreshold(gzipThreshold int) Option {
+	return func(o *options) {
+		o.GzipThreshold = gzipThreshold
+	}
+}
+
+// WithDefaultTags is used to override default tags in Client.Write methods.
+func WithDefaultTags(tags map[string]string) Option {
+	return func(o *options) {
+		o.DefaultTags = tags
+	}
+}
+
+type options struct {
+	QueryOptions
+	WriteOptions
+}
+
+func newQueryOptions(defaults *QueryOptions, opts []Option) *QueryOptions {
+	return &(newOptions(defaults, nil, opts).QueryOptions)
+}
+
+func newWriteOptions(defaults *WriteOptions, opts []Option) *WriteOptions {
+	return &(newOptions(nil, defaults, opts).WriteOptions)
+}
+
+func newOptions(defaultQueryOptions *QueryOptions, defaultWriteOptions *WriteOptions, opts []Option) *options {
+	o := &options{}
+
+	if defaultQueryOptions != nil {
+		o.QueryOptions = *defaultQueryOptions
+	}
+	if defaultWriteOptions != nil {
+		o.WriteOptions = *defaultWriteOptions
+	}
+
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	return o
 }
