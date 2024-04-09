05e2d8486135bf2a733af7a4e7e07360d6d5957e
Ales Pour <ales.pour@bonitoo.io>
2024-04-09 14:51:40 +0200

    test: add query headers test

--- influxdb3/query_test.go
+++ influxdb3/query_test.go
@@ -24,10 +24,16 @@ package influxdb3
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
+	"github.com/apache/arrow/go/v15/arrow"
+	"github.com/apache/arrow/go/v15/arrow/flight"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
 func TestQueryDatabaseNotSet(t *testing.T) {
@@ -53,4 +59,78 @@ func TestQueryWithOptionsNotSet(t *testing.T) {
 	assert.Nil(t, iterator)
 	assert.Error(t, err)
 	assert.EqualError(t, err, "options not set")
+}
+
+func TestQueryWithCustomHeader(t *testing.T) {
+	s := flight.NewServerWithMiddleware(nil)
+	err := s.Init("localhost:18080")
+	require.NoError(t, err)
+	f := &flightServer{}
+	s.RegisterFlightService(f)
+
+	go s.Serve()
+	defer s.Shutdown()
+
+	middleware := &callHeadersMiddleware{}
+	fc, err := flight.NewClientWithMiddleware(s.Addr().String(), nil, []flight.ClientMiddleware{
+		flight.CreateClientMiddleware(middleware),
+	}, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err)
+	defer fc.Close()
+
+	c, err := New(ClientConfig{
+		Host: "http://localhost:80",
+		Token: "my-token",
+		Database: "my-database",
+		Headers: http.Header{
+			"my-config-header": {"hdr-config-1"},
+		},
+	})
+	require.NoError(t, err)
+	defer c.Close()
+
+	c.setQueryClient(fc)
+
+	_, err = c.Query(context.Background(), "SELECT * FROM nothing", WithHeader("my-call-header", "hdr-call-1"))
+	_ = err // ignore it is EOF
+	assert.True(t, middleware.outgoingMDOk, "context contains outgoing MD")
+	assert.NotNil(t, middleware.outgoingMD, "outgoing MD is not nil")
+	assert.Contains(t, middleware.outgoingMD, "authorization", "auth header present")
+	assert.Contains(t, middleware.outgoingMD, "my-config-header", "custom config header present")
+	assert.Equal(t, middleware.outgoingMD["my-config-header"], []string{"hdr-config-1"}, "custom config header value")
+	assert.Contains(t, middleware.outgoingMD, "my-call-header", "custom call header present")
+	assert.Equal(t, middleware.outgoingMD["my-call-header"], []string{"hdr-call-1"}, "custom call header value")
+}
+
+// fake Flight server implementation
+
+type flightServer struct {
+	flight.BaseFlightServer
+}
+
+func (f *flightServer) DoGet(tkt *flight.Ticket, fs flight.FlightService_DoGetServer) error {
+	records := [0]arrow.Record{} // empty array will cause EOF but we do not need the result anyway, for now
+
+	w := flight.NewRecordWriter(fs)
+	for _, r := range records {
+		w.Write(r)
+	}
+
+	return nil
+}
+
+type callHeadersMiddleware struct {
+	outgoingMDOk bool
+	outgoingMD   metadata.MD
+}
+
+func (c *callHeadersMiddleware) StartCall(ctx context.Context) context.Context {
+	return ctx
+}
+
+func (c *callHeadersMiddleware) CallCompleted(ctx context.Context, err error) {
+}
+
+func (c *callHeadersMiddleware) HeadersReceived(ctx context.Context, md metadata.MD) {
+	c.outgoingMD, c.outgoingMDOk = metadata.FromOutgoingContext(ctx)
 }
