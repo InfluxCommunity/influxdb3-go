39482573de11c8f7bd49cf67792e1eb4f943f6d9
jamesbalcombe83 <james.balcombe83@gmail.com>
2024-06-12 09:05:12 +0900

    feat: add cloud dedicated database creation support

---
+++ influxdb3/management_decicated.go
@@ -0,0 +1,139 @@
+/*
+ The MIT License
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+package influxdb3
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type (
+	// Dedicatedclient represents a client for InfluxDB Cloud Dedicated administration operations.
+	DedicatedClient struct {
+		client *Client
+	}
+
+	DedicatedClientConfig struct {
+		AccountID        string
+		ClusterID        string
+		ManagementToken  string
+		ManagementAPIURL url.URL
+	}
+
+	Database struct {
+		Name               string              `json:"name"`
+		MaxTables          uint64              `json:"maxTables"`          // default 500
+		MaxColumnsPerTable uint64              `json:"maxColumnsPerTable"` // default 250
+		RetentionPeriod    uint64              `json:"retentionPeriod"`    // nanoseconds default 0 is infinite
+		PartitionTemplate  []PartitionTemplate `json:"partitionTemplate"`  // Tag or TagBucket, limit is total of 7
+	}
+
+	PartitionTemplate interface {
+		IsPartitionTemplate()
+	}
+
+	Tag struct {
+		Type  string `json:"type"`
+		Value string `json:"value"`
+	}
+
+	TagBucket struct {
+		Type  string         `json:"type"`
+		Value TagBucketValue `json:"value"`
+	}
+
+	TagBucketValue struct {
+		TagName         string `json:"tagName"`
+		NumberOfBuckets uint64 `json:"numberOfBuckets"`
+	}
+)
+
+func (t Tag) IsPartitionTemplate()        {}
+func (tb TagBucket) IsPartitionTemplate() {}
+
+// NewCloudDedicatedClient creates new DedicatedClient with given InfluxDB client.
+func NewCloudDedicatedClient(client *Client) *DedicatedClient {
+	return &DedicatedClient{client: client}
+}
+
+// CreateDatabase creates a new database
+func (d *DedicatedClient) CreateDatabase(ctx context.Context, config *DedicatedClientConfig, db *Database) error {
+	if db == nil {
+		return errors.New("database must not nil")
+	}
+
+	if d.client.config.Database == "" {
+		return errors.New("database name must not be empty")
+	}
+	db.Name = d.client.config.Database
+
+	if len(db.PartitionTemplate) > 7 {
+		return errors.New("partition template should not have more than 7 tags or tag buckets")
+	}
+
+	if db.MaxTables == 0 {
+		db.MaxTables = uint64(500)
+	}
+
+	if db.MaxColumnsPerTable == 0 {
+		db.MaxColumnsPerTable = uint64(250)
+	}
+
+	path := fmt.Sprintf("/api/v0/accounts/%s/clusters/%s/databases", config.AccountID, config.ClusterID)
+
+	return d.createDatabase(ctx, path, db, config)
+}
+
+// createDatabase is a helper function for CreateDatabase to enhance test coverage.
+func (d *DedicatedClient) createDatabase(ctx context.Context, path string, db any, config *DedicatedClientConfig) error {
+	u, err := config.ManagementAPIURL.Parse(path)
+	if err != nil {
+		return fmt.Errorf("failed to parse database creation path: %w", err)
+	}
+
+	body, err := json.Marshal(db)
+	if err != nil {
+		return fmt.Errorf("failed to marshal database creation request body: %w", err)
+	}
+
+	headers := http.Header{}
+	headers.Set("Content-Type", "application/json")
+	headers.Set("Accept", "application/json")
+	headers.Set("Authorization", "Bearer "+config.ManagementToken)
+
+	param := httpParams{
+		endpointURL: u,
+		queryParams: nil,
+		httpMethod:  "POST",
+		headers:     headers,
+		body:        bytes.NewReader(body),
+	}
+
+	_, err = d.client.makeAPICall(ctx, param)
+	return err
+}
---
+++ influxdb3/management_dedicated_test.go
@@ -0,0 +1,240 @@
+/*
+ The MIT License
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+package influxdb3
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDedicatedClientCreateDatabase(t *testing.T) {
+	correctPath := fmt.Sprintf("/api/v0/accounts/%s/clusters/%s/databases", "test-account", "test-cluster")
+
+	tests := []struct {
+		name         string
+		db           *Database
+		clientConfig *ClientConfig
+		wantBody     map[string]any
+		wantErr      bool
+	}{
+		{
+			name: "create database with name and defaults",
+			db: &Database{
+				Name:              "test-database",
+				PartitionTemplate: []PartitionTemplate{},
+			},
+			clientConfig: &ClientConfig{
+				Host:         "",
+				Token:        "my-token",
+				Organization: "default-organization",
+				Database:     "default-database",
+			},
+			wantBody: map[string]any{
+				"name":               "default-database",
+				"maxTables":          float64(500),
+				"maxColumnsPerTable": float64(250),
+				"retentionPeriod":    float64(0),
+				"partitionTemplate":  []any{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "create database with name and custom values",
+			db: &Database{
+				Name:               "test-database",
+				MaxTables:          1000,
+				MaxColumnsPerTable: 500,
+				RetentionPeriod:    1000,
+				PartitionTemplate: []PartitionTemplate{
+					Tag{
+						Type:  "tag",
+						Value: "tag-value",
+					},
+					TagBucket{
+						Type: "tag",
+						Value: TagBucketValue{
+							TagName:         "tagName",
+							NumberOfBuckets: 3,
+						},
+					},
+				},
+			},
+			clientConfig: &ClientConfig{
+				Host:         "",
+				Token:        "my-token",
+				Organization: "default-organization",
+				Database:     "default-database",
+			},
+			wantBody: map[string]any{
+				"name":               "default-database",
+				"maxTables":          float64(1000),
+				"maxColumnsPerTable": float64(500),
+				"retentionPeriod":    float64(1000),
+				"partitionTemplate": []any{
+					map[string]any{
+						"type":  "tag",
+						"value": "tag-value",
+					},
+					map[string]any{
+						"type": "tag",
+						"value": map[string]any{
+							"tagName":         "tagName",
+							"numberOfBuckets": float64(3),
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "nil database",
+			db:   nil,
+			clientConfig: &ClientConfig{
+				Host:         "",
+				Token:        "my-token",
+				Organization: "default-organization",
+				Database:     "default-database",
+			},
+			wantBody: map[string]any{},
+			wantErr:  true,
+		},
+		{
+			name: "db partition template has more than 7 tags",
+			db: &Database{
+				Name: "test-database",
+				PartitionTemplate: []PartitionTemplate{
+					Tag{},
+					Tag{},
+					Tag{},
+					Tag{},
+					Tag{},
+					Tag{},
+					Tag{},
+					Tag{},
+				},
+			},
+			clientConfig: &ClientConfig{
+				Host:         "",
+				Token:        "my-token",
+				Organization: "default-organization",
+				Database:     "default-database",
+			},
+			wantBody: map[string]any{},
+			wantErr:  true,
+		},
+		{
+			name: "empty database name",
+			db: &Database{
+				Name: "",
+			},
+			clientConfig: &ClientConfig{
+				Host:         "",
+				Token:        "my-token",
+				Organization: "default-organization",
+				Database:     "",
+			},
+			wantBody: map[string]any{},
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "empty database name" {
+				fmt.Println("empty database name")
+			}
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				// initialization of query client
+				if r.Method == "PRI" {
+					return
+				}
+
+				assert.EqualValues(t, correctPath, r.URL.String())
+				bodyBytes, err := io.ReadAll(r.Body)
+				require.NoError(t, err)
+				var body map[string]any
+				err = json.Unmarshal(bodyBytes, &body)
+				require.NoError(t, err)
+				assert.Equal(t, tt.wantBody, body)
+				w.WriteHeader(201)
+			}))
+
+			tt.clientConfig.Host = ts.URL
+			c, err := New(*tt.clientConfig)
+			require.NoError(t, err)
+
+			managementAPIURL, _ := url.Parse(ts.URL)
+			config := DedicatedClientConfig{
+				AccountID:        "test-account",
+				ClusterID:        "test-cluster",
+				ManagementToken:  "dummy",
+				ManagementAPIURL: *managementAPIURL,
+			}
+
+			dc := NewCloudDedicatedClient(c)
+			err = dc.CreateDatabase(context.Background(), &config, tt.db)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+
+	t.Run("Internal error cases", func(t *testing.T) {
+		c, err := New(ClientConfig{
+			Host:  "dummy",
+			Token: "dummy",
+		})
+		require.NoError(t, err)
+
+		managementAPIURL, _ := url.Parse(c.config.Host)
+
+		config := DedicatedClientConfig{
+			AccountID:        "test-account",
+			ClusterID:        "test-cluster",
+			ManagementToken:  "dummy",
+			ManagementAPIURL: *managementAPIURL,
+		}
+
+		dc := NewCloudDedicatedClient(c)
+		err = dc.createDatabase(context.Background(), "wrong path:", nil, &config)
+		assert.Error(t, err)
+
+		wrongBody := map[string]any{
+			"funcField": func() {},
+		}
+		err = dc.createDatabase(context.Background(), correctPath, wrongBody, &config)
+		assert.Error(t, err)
+	})
+
+}
