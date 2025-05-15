79d91a45dfe717e17a4225e56860a3c806327ba9
karel rehor <karl.koerner@bonitoo.io>
2025-05-15 10:53:24 +0200

    chore: prepare for next development iteration [skip ci]

--- influxdb3/version.go
+++ influxdb3/version.go
@@ -27,7 +27,7 @@ import (
 )
 
 // version defines current version
-const version = "2.7.0"
+const version = "2.8.0"
 
 // userAgent header value
 const userAgent = "influxdb3-go/" + version + " (" + runtime.GOOS + "; " + runtime.GOARCH + ")"
