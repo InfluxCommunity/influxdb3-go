96efc67a4f49dde044d01b00c6d34ceccf4826b7
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2025-06-26 17:50:00 +0700

    wip

--- influxdb3/client.go
+++ influxdb3/client.go
@@ -279,7 +279,7 @@ func NewFromEnv() (*Client, error) {
 
 // GetServerVersion fetches the version of the server by parsing the response headers or body of the "/ping" endpoint.
 func (c *Client) GetServerVersion() (string, error) {
-	u, err := c.apiURL.Parse("ping")
+	u, err := c.apiURL.Parse(c.config.Host + "/ping")
 	if err != nil {
 		return "", err
 	}
