e35c113b84b2b21d47902a1ebaed976647f4b028
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2026-02-24 16:14:54 +0700

    fix: improve clarity of HTTPS_PROXY override warning log

--- influxdb3/query.go
+++ influxdb3/query.go
@@ -69,10 +69,8 @@ func (c *Client) initializeQueryClient(hostPortURL string, secure bool, proxyURL
 		prevHTTPSProxy := os.Getenv("HTTPS_PROXY")
 		sanitizedInput := strings.ReplaceAll(strings.ReplaceAll(proxyURL.String(), "\n", ""), "\r", "")
 		if prevHTTPSProxy != "" && prevHTTPSProxy != proxyURL.String() {
-			slog.Warn(
-				fmt.Sprintf("Environment variable HTTPS_PROXY is already set, "+
-					"it's value will be overridden with: %s", sanitizedInput),
-			)
+			slog.Warn("Environment variable HTTPS_PROXY is already set; its value will be overridden",
+				"overridden_with", sanitizedInput)
 		}
 		err := os.Setenv("HTTPS_PROXY", proxyURL.String())
 		if err != nil {
