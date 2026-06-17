e6dbd948f8186c1f97a1efd59c16e79a8b35faf4
karel rehor <karl.koerner@bonitoo.io>
2026-06-17 14:12:22 +0200

    chore: add next release to version file and CHANGELOG.md

--- scripts/on-release.py
+++ scripts/on-release.py
@@ -6,6 +6,11 @@ import git
 
 dir_path=os.path.dirname(os.path.realpath(__file__))
 CHANGELOG=f"{dir_path}/../CHANGELOG.md"
+VERSION_FILE=f"{dir_path}/../influxdb3/version.go"
+
+TAG_MAJ = 0
+TAG_MIN = 1
+TAG_INC = 2
 
 def get_latest_tag() -> str:
     repo = git.Repo(f"{dir_path}/..")
@@ -36,10 +41,54 @@ def verify_changelog():
     print(f"Release {cl_release} on {cl_date} in CHANGELOG.md OK ✓.")
 
 
+def calculate_next_version(part=1) -> str:
+    tag_parts = re.split(r'[.-]', get_latest_tag().strip("v"))
+    tag_seps = re.split(r'[^.|^-]', get_latest_tag().strip("v"))[1:]
+
+    if part == TAG_MAJ:
+        print("incrementing major part")
+        tag_parts[TAG_MAJ] = str(int(tag_parts[TAG_MAJ]) + 1)
+    elif part == TAG_INC:
+        print("incrementing incr part")
+        tag_parts[TAG_INC] = str(int(tag_parts[TAG_INC]) + 1)
+    else:
+        print(f"incrementing minor part")
+        tag_parts[TAG_MIN] = str(int(tag_parts[TAG_MIN]) + 1)
+
+    return f"{tag_parts[TAG_MAJ]}{tag_seps[TAG_MAJ]}{tag_parts[TAG_MIN]}{tag_seps[TAG_MIN]}{tag_parts[TAG_INC]}"
+
+
+def update_version():
+    version_start_line = "const version = "
+    version_locator = re.compile(f"^{version_start_line}.*")
+    next_version = calculate_next_version()
+    next_version_line = f"{version_start_line}\"{next_version}\"\n"
+
+    with open(VERSION_FILE, "r") as fr:
+        lines = fr.readlines()
+        for i, line in enumerate(lines):
+            if version_locator.match(line):
+                lines[i] = next_version_line
+
+    with open(VERSION_FILE, "w+") as fw:
+      fw.writelines(lines)
+
+    cl_next_release_line = f"## {next_version} [unreleased]\n"
+
+    with open(CHANGELOG, "r") as fclr:
+        cl_lines = fclr.readlines()
+
+    cl_lines.insert(2, cl_next_release_line)
+    cl_lines.insert(3, "\n")
+
+    with open(CHANGELOG, "w+") as fclw:
+        fclw.writelines(cl_lines)
+
 def main():
     print("on-release start")
     print("TODO - under construction")
     verify_changelog()
+    update_version()
 
 if __name__ == "__main__":
     main()
